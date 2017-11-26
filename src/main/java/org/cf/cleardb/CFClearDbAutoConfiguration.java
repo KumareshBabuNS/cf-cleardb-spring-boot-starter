f1d9d30e8b14636bbb69fc5fdbe777c6a9d856ad
132303 <132303@INDSBLR57VL882.AMERICAS.GLOBAL.NTTDATA.COM>
2017-11-26 08:29:30 +0530

    Modified POM.xml and AutoConfiguration class.

--- src/main/java/org/cf/cleardb/CFClearDbAutoConfiguration.java
+++ src/main/java/org/cf/cleardb/CFClearDbAutoConfiguration.java
@@ -1,14 +1,21 @@
 package org.cf.cleardb;
 
-import javax.activation.DataSource;
+import javax.sql.DataSource;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @ConditionalOnClass(value= {DataSource.class})
+@Profile(value= {"cloud"})
 public class CFClearDbAutoConfiguration extends AbstractCloudConfig{
 
+	@Bean
+	public DataSource dataSource() {
+		return connectionFactory().dataSource();
+	}
 	
 }
